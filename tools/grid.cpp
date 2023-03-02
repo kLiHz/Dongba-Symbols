b678f51daa7aec52075e5142415635697fded73f
kLiHz <66966137+kLiHz@users.noreply.github.com>
2023-03-03 05:45:43 +0800

    Migrate to Boost::program_options

--- tools/grid.cpp
+++ tools/grid.cpp
@@ -1,60 +1,73 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 #include <opencv2/core/utility.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 
+#include <boost/program_options.hpp>
+
 int main(int argc, char* argv[]) {
-    auto keys =
-            "{ help h usage |             | Print this message.         }"
-            "{ display-only |             | Display by 'cv::imshow'.    }"
-            "{ thickness b  | 1           | Grid line thickness in px.  }"
-            "{ length l     | 200         | Length of a side in px.     }"
-            "{ cols c       | 4           | Number of squares in a row. }"
-            "{ rows r       | 5           | Rows of squares.            }"
-            "{ filename f   | grid.png    | Output filename.            }"
-            "{ fg           | 0   255 0   | Grid line color (RGB).      }"
-            "{ bg           | 255 255 255 | Background color (RGB).     }"
+    int cols, rows, lineWidth, length;
+    bool displayOnly;
+    std::string filename;
+    cv::Vec3b color, background;
+
+try {
+    using boost::program_options::value;
+    using boost::program_options::options_description;
+    using boost::program_options::parse_command_line;
+    using boost::program_options::variables_map;
+
+    options_description desc{"Grid Printer v0.0.1"};
+    desc.add_options()
+            ("help,h", "Print this message.")
+            ("display-only", "Display by 'cv::imshow'.")
+            ("thickness,b", value<int>()->default_value(1), "Grid line thickness in px.")
+            ("length,l", value<int>(&length)->default_value(200), "Length of a side in px.")
+            ("cols,c", value<int>(&cols)->default_value(4), "Number of squares in a row.")
+            ("rows,r", value<int>(&rows)->default_value(5), "Rows of squares.")
+            ("filename,f", value<std::string>(&filename)->default_value("grid.png"), "Output filename.")
+            ("fg", value<std::vector<int>>()->multitoken()->composing()->default_value({0,255,0}, "0 255 0"), "Grid line color (RGB).")
+            ("bg", value<std::vector<int>>()->multitoken()->composing()->default_value({255,255,255}, "255 255 255"), "Background color (RGB).")
             ;
 
-    cv::CommandLineParser parser(argc, argv, keys);
-    parser.about("Grid Printer v0.0.1");
+    variables_map vm;
 
-    if (parser.has("help")) {
-        parser.printMessage();  // Print HELP message
-        return 0;               // Then end the program, DO NOTHING
+    boost::program_options::store(parse_command_line(argc, argv, desc), vm);
+    boost::program_options::notify(vm);
+
+    if (vm.count("help")) {
+        std::cout << desc << "\n";
+        return 0;
     }
 
-    auto rows = parser.get<int>("rows");
-    auto cols = parser.get<int>("cols");
-    auto thickness = parser.get<int>("thickness");
-    auto lineWidth = 2 * thickness - 1;
-    auto length = parser.get<int>("length");
-    auto fg = parser.get<cv::Scalar>("fg");
-    auto bg = parser.get<cv::Scalar>("bg");
-    auto filename = parser.get<std::string>("filename");
-    auto displayOnly = parser.has("display-only");
+    lineWidth = vm["thickness"].as<int>() * 2 - 1;
 
+    auto f = vm["fg"].as<std::vector<int>>();
+    auto b = vm["bg"].as<std::vector<int>>();
+
+    displayOnly = vm.count("display-only");
+
+    color = cv::Vec3b(f[2], f[1], f[0]);
+    background = cv::Vec3b(b[2], b[1], b[0]);
+    
     std::cout << std::boolalpha;
     std::cout << "Rows:                     " << rows << '\n';
     std::cout << "Cols:                     " << cols << '\n';
     std::cout << "Pixel in length:          " << length << " px \n";
-    std::cout << "Grid line thickness:      " << thickness << " px \n";
     std::cout << "Grid line width:          " << lineWidth << " px \n";
-    std::cout << "Grid line color (RGB):    " << cv::Vec<int, 3>(fg[0], fg[1], fg[2]) << '\n';
-    std::cout << "Background color (RGB):   " << cv::Vec<int, 3>(bg[0], bg[1], bg[2]) << '\n';
+    std::cout << "Grid line color (RGB):    " << cv::Vec3b(f[0], f[1], f[2]) << '\n';
+    std::cout << "Background color (RGB):   " << cv::Vec3b(b[0], b[1], b[2]) << '\n';
     std::cout << "Display Only:             " << displayOnly << "\n";
     std::cout << "Output filename:          " << filename << '\n';
     std::cout << std::noboolalpha;
-
-    if (!parser.check()) {
-        parser.printErrors();
-        return 0;
+}
+    catch (const boost::program_options::error& e) {
+        std::cerr << e.what() << '\n';
+        return 1;
     }
-
-    auto color = cv::Vec<int, 3>(fg[2], fg[1], fg[0]);
-    auto background = cv::Vec<int, 3>(bg[2], bg[1], bg[0]);
 
     auto height = rows * length + (rows + 1) * lineWidth;
     auto width = cols * length + (cols + 1) * lineWidth;
